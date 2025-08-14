feb199b562b8c682714d8133621dfe3eede9c6ee
AstellisModding <astellislish@gmail.com>
2025-08-14 16:48:56 +1000

    Notes: Added notes for rebounds

--- src/main/java/net/astellismodding/golfwithmates/util/ClubUtils.java
+++ src/main/java/net/astellismodding/golfwithmates/util/ClubUtils.java
@@ -7,6 +7,13 @@ public class ClubUtils {
     public static boolean isClub(ItemStack item) {
         return item.is(ModTags.Items.GOLF_CLUBS);
     }
+
+    //todo Feat: Rebound - notes in this file
+    //this is basically a cast
+    //we can have a stamina value, this is how many blocks it needs to travel yet
+    //each block it travels it will check what block its on/in, turf is 0 friction, scruff is 1 friction, friction will devide distance by 3
+    //we can transfer the stamina value on rebound
+    //we can calculate new vector on rebound and then run this function
     public static Vec3 CalculateHitResultLocation(float x, float z, float r, double v, int driveType) {
         double power = 16 * v * driveType;
         float rot = r;
